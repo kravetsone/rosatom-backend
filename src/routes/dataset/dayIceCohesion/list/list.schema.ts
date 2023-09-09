fde534cfba3f07026ac35ef20f28078cc626740b
kravetsone <57632712+kravetsone@users.noreply.github.com>
2023-09-09 08:45:32 +0000

    feat: add list operation to dataset models

---
+++ src/routes/dataset/dayIceCohesion/list/list.schema.ts
@@ -0,0 +1,35 @@
+import z from "zod";
+
+const querystring = z.object({
+    page: z.string().transform(Number).refine(Boolean).default("1"),
+    pageSize: z.string().transform(Number).refine(Boolean),
+});
+
+const userNotExists = z.object({
+    code: z.string().default("USER_NOT_EXISTS"),
+    message: z.string().default("Введённые вами логин или пароль неверны"),
+});
+
+const response = z
+    .object({
+        pageCount: z.number(),
+        items: z.array(
+            z.object({
+                id: z.number(),
+                date: z.string(),
+                iceCohesion: z.number(),
+            }),
+        ),
+    })
+    .describe("Список");
+
+export const schema = {
+    description: "Получение списка",
+    tags: ["dayIceCohesion"],
+    security: [{ bearerAuth: [] }],
+    querystring,
+    response: {
+        200: response,
+        401: userNotExists,
+    },
+};
---
+++ src/routes/dataset/edgeAvailability/list/list.schema.ts
@@ -0,0 +1,36 @@
+import { IceClass } from "@prisma/client";
+import z from "zod";
+
+const querystring = z.object({
+    page: z.string().transform(Number).refine(Boolean).default("1"),
+    pageSize: z.string().transform(Number).refine(Boolean),
+});
+
+const userNotExists = z.object({
+    code: z.string().default("USER_NOT_EXISTS"),
+    message: z.string().default("Введённые вами логин или пароль неверны"),
+});
+
+const response = z
+    .object({
+        pageCount: z.number(),
+        items: z.array(
+            z.object({
+                isFeasible: z.boolean(),
+                iceBreakerName: z.string(),
+                id: z.number(),
+            }),
+        ),
+    })
+    .describe("Список");
+
+export const schema = {
+    description: "Получение списка",
+    tags: ["edgeAvailability"],
+    security: [{ bearerAuth: [] }],
+    querystring,
+    response: {
+        200: response,
+        401: userNotExists,
+    },
+};
